4fd0ce69f774617648fc604165ef89e081e93a60
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-08-13 14:08:03 +0700

    Add project files.

---
+++ PDGICoreApps/Controllers/HelloController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDGICoreApps.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.FirstName = "Erick";
+            ViewBag.LastName = "Kurniawan";
+            ViewData["Email"] = "erick@actual-training.com";
+            return View();
+        }
+
+        public IActionResult Contact(string firstname,string lastname,string gender,
+            string[] language,string country,string summary,DateTime birthdate,int quality)
+        {
+            ViewData["firstname"] = firstname;
+            ViewData["lastname"] = lastname;
+            ViewData["gender"] = gender;
+            ViewData["language"] = language;
+            ViewData["country"] = country;
+            ViewData["summary"] = summary;
+            ViewData["birthdate"] = birthdate;
+            ViewData["quality"] = quality;
+            //return Content(birthdate.ToString());
+
+            return View();
+        }
+    }
+}
